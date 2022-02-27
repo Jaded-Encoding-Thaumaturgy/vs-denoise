0422262792a1086b50caead547f10d8d331a4a81
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-02-27 20:37:59 +0100

    Create mvtools

---
+++ vsdenoise/mvtools.py
@@ -0,0 +1,14 @@
+"""
+This module implements wrappers for mvtool
+"""
+
+from __future__ import annotations
+
+import vapoursynth as vs
+
+core = vs.core
+
+
+class SMDegrain:
+    """Simple MVTools Degrain with motion analysis"""
+    ...
