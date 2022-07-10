0468132c901ed297e9768ddd1b53b7700d94aeea
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-10 16:33:02 +0200

    Remove old utils

--- vsdenoise/utils.py
+++ vsdenoise/utils.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from typing import List, Sequence, TypeVar
 
 import vapoursynth as vs
-from vsutil import get_depth
+from vsrgtools.util import PlanesT, normalise_planes
+
+from vsdenoise.knlm import ChannelMode
 
 T = TypeVar('T')
 
@@ -12,15 +14,3 @@ def arr_to_len(array: Sequence[T], length: int = 3) -> List[T]:
     return (list(array) + [array[-1]] * length)[:length]
 
 
-# here until vsutil gets a new release
-def get_peak_value(clip: vs.VideoNode, chroma: bool = False) -> float:
-    assert clip.format
-    return (0.5 if chroma else 1.) if clip.format.sample_type == vs.FLOAT else (1 << get_depth(clip)) - 1.
-
-
-def get_neutral_value(clip: vs.VideoNode, chroma: bool = False) -> float:
-    assert clip.format
-
-    is_float = clip.format.sample_type == vs.FLOAT
-
-    return (0. if chroma else 0.5) if is_float else float(1 << (get_depth(clip) - 1))
