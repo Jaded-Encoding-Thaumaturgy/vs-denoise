48581a30756258a9fc3fdeffabff022c9610f9d5
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-10 16:35:18 +0200

    Only accept normalized planes in planes_to_channelmode

--- vsdenoise/utils.py
+++ vsdenoise/utils.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from typing import List, Sequence, TypeVar
 
-from vsrgtools.util import PlanesT
-
 from vsdenoise.knlm import ChannelMode
 
 T = TypeVar('T')
@@ -13,7 +11,7 @@ def arr_to_len(array: Sequence[T], length: int = 3) -> List[T]:
     return (list(array) + [array[-1]] * length)[:length]
 
 
-def planes_to_channelmode(planes: PlanesT) -> ChannelMode:
+def planes_to_channelmode(planes: List[int]) -> ChannelMode:
     if planes == [0]:
         return ChannelMode.LUMA
 
