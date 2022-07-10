ab3fc755d0d15d0f863e38e8ac8588f98c33ebbd
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-10 15:06:27 +0200

    Add metadata file, update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,42 @@
 #!/usr/bin/env python3
 
 import setuptools
+from pathlib import Path
 
-with open("requirements.txt") as fh:
-    install_requires = fh.read()
+package_name = 'vsdenoise'
 
-name = "vs-denoise"
-version = "0.0.0"
+exec(Path(f'{package_name}/_metadata.py').read_text(), meta := dict[str, str]())
+
+readme = Path('README.md').read_text()
+requirements = Path('requirements.txt').read_text()
+
 
 setuptools.setup(
-    name=name,
-    version=version,
-    author="Irrational Encoding Wizardry",
-    author_email="wizards@encode.moe",
-    description="Vapoursynth denoising functions",
-    packages=["vsdenoise"],
-    url="https://github.com/Irrational-Encoding-Wizardry/vs-denoise",
-    package_data={
-        'vsdenoise': ['py.typed'],
+    name=package_name,
+    version=meta['__version__'],
+    author=meta['__author_name__'],
+    author_email=meta['__author_email__'],
+    maintainer=meta['__maintainer_name__'],
+    maintainer_email=meta['__maintainer_email__'],
+    description=meta['__doc__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    project_urls={
+        'Source Code': 'https://github.com/Irrational-Encoding-Wizardry/vs-denoise',
+        'Documentation': 'https://vsdenoise.encode.moe/en/latest/',
+        'Contact': 'https://discord.gg/qxTxVJGtst',
     },
-    install_requires=install_requires,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    install_requires=requirements,
     python_requires='>=3.10',
+    packages=[
+        package_name
+    ],
+    package_data={
+        package_name: ['py.typed']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
 )
---
+++ vsdenoise/_metadata.py
@@ -0,0 +1,12 @@
+"""VapourSynth denoising functions"""
+
+__version__ = '0.0.0'
+
+__author__ = 'Irrational Encoding Wizardry <wizards@encode.moe>'
+__maintainer__ = __author__
+
+__author_name__, __author_email__ = [x[:-1] for x in __author__.split('<')]
+__maintainer_name__, __maintainer_email__ = [x[:-1] for x in __maintainer__.split('<')]
+
+if __name__ == '__github__':
+    print(__version__)
