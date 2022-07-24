3a558eb84d2e8bb86c89ad554389c740f959161d
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-24 03:09:30 +0200

    Update build system (#6)
    * Update gitignore
    
    * Merge linting files in one
    
    * Add dependabot
    
    * Update linting action
    
    * Add metadata file, update setup.py
    
    * Create README.md
    
    * Fix typo in lint.yml
    
    * Only run mypy lint if dependecies are installed correctly
    
    * Update metadata

---
+++ vsdenoise/_metadata.py
@@ -0,0 +1,12 @@
+"""VapourSynth denoising functions"""
+
+__version__ = '0.0.0'
+
+__author_name__, __author_email__ = 'Irrational Encoding Wizardry', 'wizards@encode.moe'
+__maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
+
+__author__ = f'{__author_name__} <{__author_email__}>'
+__maintainer__ = __author__
+
+if __name__ == '__github__':
+    print(__version__)
