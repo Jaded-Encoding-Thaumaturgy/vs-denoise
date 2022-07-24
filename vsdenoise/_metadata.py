68a366304d5dc2631b17a571383590b5bfc7d9b4
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-24 03:08:05 +0200

    Update metadata

--- vsdenoise/_metadata.py
+++ vsdenoise/_metadata.py
@@ -2,11 +2,11 @@
 
 __version__ = '0.0.0'
 
-__author__ = 'Irrational Encoding Wizardry <wizards@encode.moe>'
-__maintainer__ = __author__
+__author_name__, __author_email__ = 'Irrational Encoding Wizardry', 'wizards@encode.moe'
+__maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
 
-__author_name__, __author_email__ = [x[:-1] for x in __author__.split('<')]
-__maintainer_name__, __maintainer_email__ = [x[:-1] for x in __maintainer__.split('<')]
+__author__ = f'{__author_name__} <{__author_email__}>'
+__maintainer__ = __author__
 
 if __name__ == '__github__':
     print(__version__)
