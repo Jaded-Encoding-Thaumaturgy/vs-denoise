b2bcfac74fa50639872328220ddc18fa5ddae516
Setsugennoao <setsugen@setsugen.dev>
2023-02-23 16:36:34 +0100

    Update prefilters.py

--- vsdenoise/prefilters.py
+++ vsdenoise/prefilters.py
@@ -22,8 +22,7 @@ from vstools import (
 
 from .bm3d import BM3D as BM3DM
 from .bm3d import BM3DCPU, AbstractBM3D, BM3DCuda, BM3DCudaRTC, Profile
-# from .dfttest import DFTTest
-from vsdenoise import DFTTest
+from .dfttest import DFTTest
 from .knlm import DEVICETYPE, DeviceType, nl_means
 
 __all__ = [
