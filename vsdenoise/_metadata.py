43155df295dd1049ddd2560c69bd5421d3fce73d
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-02-26 23:29:53 +0100

    Bump major version

--- vsdenoise/_metadata.py
+++ vsdenoise/_metadata.py
@@ -1,6 +1,6 @@
 """VapourSynth denoising functions"""
 
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 
 __author_name__, __author_email__ = 'Irrational Encoding Wizardry', 'wizards@encode.moe'
 __maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
