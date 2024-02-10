f0366dfaf5f50493b35303808b6c43e86c55e4b6
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-02-10 09:19:00 +0100

    dvd_deblur: New function
    Co-Authored-By: wiwaz <105133551+wiwaz@users.noreply.github.com>

--- vsdenoise/freqs.py
+++ vsdenoise/freqs.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from typing import Any, Iterable
+from typing import Any, Callable, Iterable
 
+from vskernels import FmtConv
 from vsrgtools import MeanMode
 from vstools import (
     CustomValueError, GenericVSFunction, KwargsT, PlanesT, fallback, flatten_vnodes, get_video_format, normalize_seq, vs
@@ -11,7 +12,8 @@ from .fft import DFTTest
 from .mvtools import MVTools
 
 __all__ = [
-    'frequency_merge'
+    'frequency_merge',
+    'dvd_deblur'
 ]
 
 
@@ -66,3 +68,54 @@ def frequency_merge(
             high_freqs = mv.compensate(mode_high, ref=high_freqs)  # type: ignore
 
     return low_freqs.std.MergeDiff(high_freqs)
+
+
+def dvd_deblur(
+    clip: vs.VideoNode, clean: vs.VideoNode,
+    blur: tuple[float, float] | float, taps: int = 4,
+    post_process: Callable[[vs.VideoNode, vs.VideoNode], vs.VideoNode] = None,
+    compare: bool = False,
+) -> vs.VideoNode:
+    """
+    Function to deblur DVDs using a reference clean source.
+
+    Blurring on JP DVDs is oftentimes the result of horizontal resampling
+    (anamorphic -> square pixels -> anamorphic) using Lanczos with a blur value.
+    This is usually not the case for DVDs from other regions.
+
+    This function works by performing the same resampling on the "clean" clip,
+    and creating a diff clip to try and restore the original clip.
+
+    :param clip:            Clip to process.
+    :param clean:           "Clean" clip that does not have the resample blurring.
+    :param blur:            Blur values to pass on to Lanczos. As blur values may differ
+                            between the resampling steps, a tuple of floats is accepted.
+                            If only 1 float is passed, it will be used for both steps.
+    :param taps:            Taps param for the Lanczos kernel. Default: 4.
+    :param post_process:    Function for performing post-processing on the clip prior to the diffing step.
+                            The function must accept two input clips (clip, mangled) and return a clip.
+                            Default: None.
+    :param compare:         Return the mangled clip so it can be compared to the source clip. Default: False.
+
+    :return:                Demangled input clip.
+    """
+
+    if not isinstance(blur, tuple):
+        blur = (blur, blur)
+
+    class FmtConvLanczos(FmtConv):
+        _kernel = "lanczos"
+
+    lpf = FmtConvLanczos(taps)
+
+    mangle = lpf.scale(clean, clip.width, clip.height, fh=1 / blur[0])
+    mangle = lpf.scale(mangle, clip.width, clip.height, fh=1 / blur[1])
+
+    if compare:
+        return mangle
+
+    if post_process is not None:
+        clip = post_process(clip, mangle)
+
+    diff = clean.std.MakeDiff(mangle)
+    return clip.std.MergeDiff(diff)
